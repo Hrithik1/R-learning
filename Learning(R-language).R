ba6fceb36d8f589e23ebd9b60091ddec16eb9300
Hrithik Rana <hrithik2769168@gmail.com>
2021-01-03 18:06:33 +0530

    R language(Basics)

---
+++ Learning(R-language).R
@@ -0,0 +1,23 @@
+# Learning R-language
+
+x<-factor(c("Male","Female","Female","Other","Male","Other"),levels=c("Male","Female","Other"))
+x
+table(x)
+unclass(x)
+attributes(x)
+dim(x)
+
+#---------------
+
+x<-matrix(nrow=5,ncol=2,1:10)
+x
+
+table(x)
+unclass(x)
+attributes(x)
+dim(x)
+
+#---------------
+  
+
+
